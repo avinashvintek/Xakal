7ba708fca2030b48bd41e4b443e91b893e7235f8
Asfiya <asfiya@vinteksolution.com>
2019-12-14 18:28:02 +0530

    Salary - Bug fixes

--- xakal/services/controllers/salaryController.js
+++ xakal/services/controllers/salaryController.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var { SalaryDetails } = require('../models/staff-portal/salary.model.js');
 
 router.get('/salarydetail', (req, res) => {
-    SalaryDetails.find(req.params, { _id: 0 }).then((eachOne) => {
+    const filter = { salaryYear: req.query.salaryYear, salaryMonth: req.query.salaryMonth, userID: req.query.userID };
+    SalaryDetails.find(filter, { _id: 0 }).then((eachOne) => {
         res.json(eachOne)
     })
 });
--- xakal/src/components/staff-portal/salary-details.js
+++ xakal/src/components/staff-portal/salary-details.js
@@ -131,7 +131,7 @@ class SalaryDetails extends Component {
      */
     fetchSalaryDetails() {
         this.setState({ searchAllowed: true });
-        var params = { month: this.state.selectedMonth, year: this.state.selectedYear, userID: this.state.userID.toUpperCase() };
+        var params = { salaryMonth: this.state.selectedMonth, salaryYear: this.state.selectedYear, userID: this.state.userID.toUpperCase() };
         axios.get(`http://localhost:4000/xakal/salary/salarydetail`, { params: params })
             .then((response) => {
                 this.setState({ salaryDetails: response.data });
