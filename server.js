58cd7413439b979f84fa41d1aea7981581bcf4f8
Asfiya <asfiya@vinteksolution.com>
2020-10-22 11:42:13 +0530

    #Xakal - Build issue fix

--- server.js
+++ server.js
@@ -53,8 +53,9 @@ app.use('/xakal/follower', followerController);
 app.use('/xakal/following', followingController);
 
 if (process.env.NODE_ENV === 'production') {
-    // app.use(express.static("client/build"));
-    app.use('/static', express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static("client/build"));
+    // app.use('/static', express.static(path.join(__dirname, 'client/build')));
+    // express.static(path_join(__dirname, '/client/build'))
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
